ac0d4e1f47d8a5b844a3cc726b85fe6bac2a2c25
Asianguy123 <sas@drsyed.org>
2022-02-10 21:46:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -25,33 +25,6 @@ def check_move(move):
         print('Invalid move, choose elsewhere.')
     return valid_move
 
-# Noughts and Crosses (Tic Tac Toe)
-
-# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# Imports 
-
-import random
-from random import randint
-
-# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# Move Input Functions
-
-def input_move():
-    move = int(input('Enter number 1-9:  '))
-    move -= 1
-    valid_move = check_move(move)
-    if valid_move:
-        grid[move] = 'O'
-    return valid_move
-
-def check_move(move):
-    valid_move = False
-    if grid[move] == ' ':
-        valid_move = True
-    else:
-        print('Invalid move, choose elsewhere.')
-    return valid_move
-
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Cell + Row Value Calculation Functions  
 
